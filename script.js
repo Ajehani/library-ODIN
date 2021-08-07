79d38d24817d355fc04320c649f6e202d098cdba
Anas Jehani <creativelybankrupt02@gmail.com>
2021-08-07 03:13:07 +0100

    local storage,remove button,read toggle button, styling improvements +bug fixes

--- script.js
+++ script.js
@@ -1,64 +1,96 @@
 let myLibrary=[]
 let form
 const display=document.querySelector('#display');
-let books=document.querySelector('#new');
-const modal=document.querySelector('.modal')
+let button=document.querySelector('#new');
+const modal=document.querySelector('#modal')
 const submit=modal.querySelector('#submit')
 const overlay=document.querySelector('#overlay')
-let title=modal.querySelector("#Title")
-let author=modal.querySelector("#Author")
-let pages=modal.querySelector("#Pages")
+let title=modal.querySelector("#title")
+let author=modal.querySelector("#author")
+let pages=modal.querySelector("#pages")
+let ifread=modal.querySelectorAll('input[name="ifread"]')
 let close=modal.querySelector('#close')
-pages.innerHTML="yo";
 
-function Books(author,title,pages){
+function Books(author,title,pages,status){
     this.author=author,
     this.title=title,
-    this.pages=pages;
-    // this.status=status;
+    this.pages=pages,
+    this.status=status;
 }
+let data=JSON.parse(localStorage.getItem("Library"))
+for(let i of data){
+    add(i)
+}
+console.log('this data',data)
 overlay.addEventListener('click',()=>{
     modal.setAttribute("style","display:none;")
     overlay.setAttribute("style","opacity:0;")
-
 })
 close.addEventListener('click',()=>{
     modal.setAttribute("style","display:none;")
     overlay.setAttribute("style","opacity:0;")
 })
-books.addEventListener('click',function(){
-    // form=document.createElement('div')
-    // form.className="modal"
-    // display.appendChild(form)
+button.addEventListener('click',()=>{
     overlay.setAttribute("style","opacity:1;pointer-events:all;")
     modal.setAttribute("style","display:flex;")
 })
-submit.addEventListener('click',function(){
-    let book=new Books(author,title,pages)
-    console.table(book)
+submit.addEventListener('click',(e)=>{
+    let selected
+    for(let i of ifread){
+        if(i.checked){
+            selected=i.value
+        }
+    }
+    e.preventDefault();
+    let book=new Books(author.value,title.value,pages.value,selected)
     add(book)
-    title.textContent=''
-    author.textContent=''
-    pages.textContent=''
+    title.value=" "
+    author.value=" "
+    pages.value=" "
     overlay.setAttribute('style',"opacity:0;")
     modal.setAttribute('style',"display:none;")
 })
-let count=0
+
 function add(b){
-    count++
+    let child=document.createElement("li")
+    let btn=document.createElement("button")
+    let check=document.createElement("button")
+    check.type="button";
+    child.setAttribute("id","child")
+    btn.setAttribute("id","del")
+    check.setAttribute("id","check")
+    btn.innerHTML='&times;'
+    check.textContent="Change Reading Status"
+    child.textContent=`Title:${b.title} - Author:${b.author} - Pages:${b.pages} - Have you read it?:${b.status}`
+    child.appendChild(btn)
+    child.appendChild(check)
+    display.appendChild(child) 
+
+    btn.addEventListener('click',()=>{
+        child.remove()
+        btn.remove()
+        myLibrary=myLibrary.filter(n=>n!=b)
+        localStorage.clear()
+        localStorage.setItem("Library",JSON.stringify(myLibrary))
+    })
+    check.addEventListener('click',()=>{
+        if(b.status==="Yes"){
+            b.status="No"
+        }
+        else{
+            b.status="Yes"
+    }
+    child.textContent=`Title:${b.title} - Author:${b.author} - Pages:${b.pages} - Have you read it?:${b.status}`
+    child.appendChild(btn)
+    child.appendChild(check)
+    myLibrary[myLibrary.indexOf(b)]=b
+    localStorage.clear()
+    localStorage.setItem("Library",JSON.stringify(myLibrary))
+})
     myLibrary.push(b)
-    display.innerText+=`\n ${count}\\${myLibrary}`
-    // let child=document.createElement("div")
-    // display.appendChild(child)
-    // child.textContent= + b
-    
     console.log(myLibrary)
+    localStorage.setItem("Library",JSON.stringify(myLibrary))
+    
 }
 
-// function tab(){
-//     for(let i in myLibrary){
-//         display.textContent+=`${i}:${myLibrary[i]}.`
-//         display.textContent+="\t"
-//     } 
-//     console.table(myLibrary)
-// }
+
